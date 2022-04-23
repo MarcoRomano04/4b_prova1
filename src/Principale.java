3e1e097f60f15a9a699901d9fbe0fd024e322424
Marco 2 <Eliana@DESKTOP-ME1M4E6>
2022-04-23 12:01:37 +0200

    versione 2

--- src/Principale.java
+++ src/Principale.java
@@ -2,13 +2,13 @@
  * 
  * @author Marco
  * 
- * @version 1.00
+ * @version 2.00
  * 
  */
 public class Principale {
 
 	public static void main(String[] args) {
-		System.out.println("Prova GitHub");
+		System.out.println("Prova GitHub 2");
 	}
 
 }
